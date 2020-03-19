49cfe806a9074833e65d6c0d278d73ac18646bc8
hayakawa1234 <61731486+hayakawa1234@users.noreply.github.com>
2020-03-19 18:42:58 +0900

    新しい演習

---
+++ Assets/GameFinish.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameFinish : MonoBehaviour
+{
+    private int bestScore;
+    public Text finishTime;
+    public Text bestTime;
+    public GameObject finishUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("BestScore"))
+        {
+            bestScore = PlayerPrefs.GetInt("BestScore");
+        }
+        else
+        {
+            bestScore = 999;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Goal.goal == true)
+        {
+            finishUI.SetActive(true);
+            int score = Mathf.FloorToInt(Timer.time);
+            finishTime.text = "FinishTime = " + score;
+            bestTime.text = "BestTime = " + bestScore;
+            if(bestScore > score)
+            {
+                PlayerPrefs.SetInt("BestScore", score);
+            }
+        }
+    }
+    public void OnRetry()
+    {
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
+    }
+    
+}
---
+++ Assets/Out.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Out : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(
+                SceneManager.GetActiveScene().name);
+        }
+        
+    }
+}
