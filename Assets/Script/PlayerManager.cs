3fec41771bce3ceeadc92dcf6e37c1eba3154e1b
Satoru Hayakawa <hayatan5152@gmail.com>
2020-06-14 17:15:19 +0900

    unity 新しいバージョン
    導入

---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{   
+    //Rigidbodyの変数rbの宣言
+    Rigidbody rb;
+    float x;
+    float z;
+    public float moveSpeed = 2;
+    void Start()
+    {
+        //Rigidbodyの値を取得
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キーボード入力で移動
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+
+ 
+
+    }
+
+    private void FixedUpdate()
+    {
+        //速度設定
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
+    }
+}
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 1.UIシステムを利用するときに利用（今回でいう「Text」）
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    // 2.時間経過を示す「time」変数
+    public static float time;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // 3.ゲーム開始時は「time」は「0」
+        time = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 4.まだゴールをしてなかったら
+        if (Goal.goal == false)
+        {
+            // 5.経過時間を追加
+            time = time + Time.deltaTime;
+        }
+
+        // 6.小数点以下を切り捨てる
+        int t = Mathf.FloorToInt(time);
+
+        // 7.「Text」Componentを取得して、「uiText」に格納
+        Text uiText = GetComponent<Text>();
+
+        // 8.テキストを編集
+        uiText.text = "Time = " + t;
+    }
+}
