eb27ea067b18fdca15cfc1a403a9c8a2e72f8332
hayakawa1234 <hayakawa@oysterworld.jp>
2020-06-19 14:43:06 +0900

    動かせるキューブの回転実装
    transformを用いてキューブを回転させた。

---
+++ Assets/Script/SphereObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereObject : MonoBehaviour
+{
+    void Update()
+    {
+        Transform sphereTransform = this.transform;
+
+        sphereTransform.Rotate(1.0f, 1.0f, 1.0f, Space.World);
+    }
+}
