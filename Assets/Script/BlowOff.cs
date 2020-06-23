d3bb6e3710f459bb15affd0c13cd06228ea84062
hayakawa1234 <hayakawa@oysterworld.jp>
2020-06-23 17:45:58 +0900

    hpが0になった時に吹き飛ばすBlowOffの実装
    アニメーションが当たった時にhpが0の場合AddForceメソッドを使って飛ぶように実装した。

---
+++ Assets/Script/BlowOff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowOff : MoveBoxBase
+{ 
+    //特定のアニメーションが当たった場合の処理
+    public void OnTriggerEnter(Collider other)
+    {
+        Damager damager = other.GetComponent<Damager>();
+        if (damager != null)
+        {
+            Damage(damager.damage);
+
+            //hpが0になった時にボックスを飛ばす処理
+            if (hp <= 0)
+            {
+                Rigidbody rb = GetComponent<Rigidbody>();
+                Vector3 force = new Vector3(20.0f, 20.0f, 0.0f);
+                rb.AddForce(force, ForceMode.Impulse);
+            }
+        }
+    }
+
+    }
