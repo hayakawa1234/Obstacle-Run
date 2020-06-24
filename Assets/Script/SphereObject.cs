d8cacd7a63cb74fb1bb7bb54be1e1396e14c6e3d
hayakawa1234 <hayakawa@oysterworld.jp>
2020-06-24 14:11:55 +0900

    sphereobjectを回転だけでなく剣が当たった時に大きくなるように実装
    剣が当たった時に大きくなるように実装。
    大きくなった後に剣が当たったときに吹き飛ぶように実装した。

--- Assets/Script/SphereObject.cs
+++ Assets/Script/SphereObject.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereObject : MonoBehaviour
+public class SphereObject : MoveBoxBase
 {
     void Update()
     {
         Transform sphereTransform = this.transform;
 
         sphereTransform.Rotate(1.0f, 1.0f, 1.0f, Space.World);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Damager damager = other.GetComponent<Damager>();
+        if (damager != null)
+        {
+            Damage(damager.damage);
+
+            //hpが0になった時にボックスを飛ばす処理
+            if (hp <= 0)
+            {
+                this.transform.localScale = new Vector3(4, 4, 4);
+            }
+
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 force = new Vector3(3.0f, 3.0f, 3.0f);
+            rb.AddForce(force, ForceMode.Impulse);
+        }
     }
 }
