577659a52febd14ea81dd58be6413c80b4baa625
hayakawa1234 <hayakawa@oysterworld.jp>
2020-06-24 18:35:01 +0900

    Titleシーンの実装
    キャラクターの配置
    テキストの配置
    空の設定
    地面の設定
    シーン遷移をするTitleScene.csの作成

---
+++ Assets/Script/TitleScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//シーンマネジメントを有効にする
+
+public class TitleScene : MonoBehaviour
+{
+	//シーンの遷移処理
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.B)) //マウス左クリック,Bボタンを押した場合
+		{
+			SceneManager.LoadScene("main");//mainシーンをロードする
+		}
+
+	}
+}
