825d519f8e94b691902949f7df59860a85b0d53c
hayakawa1234 <hayakawa@oysterworld.jp>
2020-06-24 14:09:50 +0900

    剣が当たった時にBoxが小さくなるSmallBox.csを実装
    剣が当たった時にBoxが小さくなるように実装した。

---
+++ Assets/Script/SmallBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallBox : MoveBoxBase
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        Damager damager = other.GetComponent<Damager>();
+        if (damager != null)
+        {
+            Damage(damager.damage);
+
+            //hpが0になった時にボックスを飛ばす処理
+            if (hp <= 0)
+            {
+                this.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            }
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 force = new Vector3(10.0f, 10.0f, 10.0f);
+            rb.AddForce(force, ForceMode.Impulse);
+        }
+    }
+}
