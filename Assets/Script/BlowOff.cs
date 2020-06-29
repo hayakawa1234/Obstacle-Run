317c5e7307f5a3562eefdf6604b2be5379d6e4f3
hayakawa1234 <hayakawa@oysterworld.jp>
2020-06-29 11:55:29 +0900

    BlowOff.csに衝突したときに爆発するエフェクトを追加
    BlowOff.csに衝突したときに爆発するエフェクトを追加

--- Assets/Script/BlowOff.cs
+++ Assets/Script/BlowOff.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class BlowOff : MoveBoxBase
 {
+    public GameObject particleObject;
     //特定のアニメーションが当たった場合の処理
     public override void OnTriggerEnter(Collider other)
     {
+        Instantiate(particleObject, this.transform.position, Quaternion.identity); 
         Rigidbody rb = GetComponent<Rigidbody>();
         Vector3 force = new Vector3(0.0f, 5.0f, 5.0f);
         rb.AddForce(force, ForceMode.Impulse);
