be46957e56a02aa0d962cc71f764ecc8275168e7
hayakawa1234 <hayakawa@oysterworld.jp>
2020-06-29 18:01:38 +0900

    Test Runnerの導入
    PlayModeとEditModeに分けて作成

---
+++ Assets/Tests/EditMode/Editor/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Player
+    {
+        [Test]
+        public void FixedUpdate()
+        {
+            
+        }
+
+
+
+        [Test]
+        public void SetLocalGravity()
+        {
+            
+        }
+
+        [Test]
+        public void ShowColliderWeapon()
+        {
+
+        }
+    }
+}
